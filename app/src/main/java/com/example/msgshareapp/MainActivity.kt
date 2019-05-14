fd1173b114abd45ffa8d4046e87f6e9dc98847b4
Elang Surya <elangsurya19@gmail.com>
2019-05-14 19:46:57 +0700

    Section Two

--- app/src/main/java/com/example/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/example/msgshareapp/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
             Log.i("MainActivity", "Button was clicked !")
 
             Toast.makeText(this, "Button was clicked !", Toast.LENGTH_SHORT).show()
+
         }
     }
 }
