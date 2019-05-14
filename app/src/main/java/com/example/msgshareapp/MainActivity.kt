da3f4e0ee1e5296cc390582c7fe3747975c87733
Elang Surya <elangsurya19@gmail.com>
2019-05-14 20:57:13 +0700

    Section Two, video tree

--- app/src/main/java/com/example/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/example/msgshareapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.msgshareapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
@@ -13,11 +14,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btnShowToast.setOnClickListener {
-            // Code
             Log.i("MainActivity", "Button was clicked !")
-
             Toast.makeText(this, "Button was clicked !", Toast.LENGTH_SHORT).show()
+        }
 
+        btnSendMsgToNextActivity.setOnClickListener {
+            val message: String = etUserMessage.text.toString()
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
         }
     }
 }
